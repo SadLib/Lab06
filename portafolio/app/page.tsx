e824bf22e6dc21d075a475c58064f1adbf0c9df8
neftali-libonatti <149745977+SadLib@users.noreply.github.com>
2026-04-11 04:06:32 +0000

    Interests

--- portafolio/app/page.tsx
+++ portafolio/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import HeaderModule from "./Componentes/Header/Header"
 import HeroModule from "./Componentes/Hero/Hero"
+import Interests from "./Componentes/Interests/Interests";
+
 export default function Home() {
   return (
     <>
     <HeaderModule />
     <HeroModule />
+    <Interests />
+  
     </>
   );
 }
